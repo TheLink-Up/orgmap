607340986a4d02fddf2d9b3be8d8026745c219b5
Tyghe Vallard <vallardt@gmail.com>
2015-03-10 06:03:36 -0400

    very close

--- orgmap.js
+++ orgmap.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
 /* 
   Google Maps Javascript API Key
   I don't know how to get this to dynamically get
@@ -7,6 +6,7 @@
 var APIKEY = "";
 /* Page url to fetch organizations from */
 var ORGPAGEURL = "";
+var ORGMAPAPI = "";
 /* The div id to put the map into */
 var MAPCANVASDIV = "map-canvas";
 /* Center coordinates to load for the initial map load */
@@ -17,14 +17,6 @@ var MAPCENTER = { lat: 50, lng: -114};
   12 -> Closest zoom level
 */
 var MAPZOOMLEVEL = 3;
-</script>
-<script type="text/javascript"
-  src="https://maps.googleapis.com/maps/api/js?key=APIKEY">
-</script>
-<script type="text/javascript"
-  src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
-</script>
-<script type="text/javascript">
   function createMap(divid, map_options) {
     var map = new google.maps.Map(
         document.getElementById(divid),
@@ -49,40 +41,48 @@ var MAPZOOMLEVEL = 3;
   [4] -> Link name
   */
   var org_markers = [];
-  function make_org_marker(org_link) {
-    var geo = new google.maps.Geocoder();
-    var citystate = org_link[2] + ',' + org_link[3];
-    console.log(citystate);
-    geo.geocode(
-      {address: citystate},
-      function(georesult, geostatus) {
-        console.log(geostatus);
-        console.log(georesult[0]);
-        var latlng = georesult[0].geometry.location;
-        console.log(org_link[4]);
-        console.log(latlng);
-        /*
-        var attribution = new google.maps.Attribution({
-          source: org_link[4],
-          webUrl: org_link[1]
-        });
-        */
-        var orgmarker = new google.maps.Marker({
-          map: orgmap,
-          position: latlng,
-          title: org_link[4]
-          /*attribution: attribution*/
-        });
-        org_markers.push(orgmarker);
-        google.maps.event.addListener(orgmarker, 'click', function() {
-          org_info_window.setContent(
-            "<a href=\""+ org_link[1] + "\" target=\"_blank\">" + org_link[4] + "</a>"
-          );
-          org_info_window.open(orgmap, orgmarker);
-        });
-      }
-    )
-  }
+function make_org_marker(org_link) {
+    $.ajax({
+        url: ORGMAPAPI,
+        jsonp: "callback",
+        dataType: "jsonp",
+        data: {
+            url: org_link[1],
+            city: org_link[2],
+            state: org_link[3],
+            name: org_link[4]
+        },
+        success: function(response) {
+            console.log("Jsonp response:")
+            console.log(response);
+            var j = JSON.parse(response);
+            /*
+            var attribution = new google.maps.Attribution({
+              source: org_link[4],
+              webUrl: org_link[1]
+            });
+           */
+            var latlng = {
+                lat: j.lat,
+                lng: j.lng
+            };
+            var orgmarker = new google.maps.Marker({
+              map: orgmap,
+              position: latlng,
+              title: org_link[4]
+              /*attribution: attribution*/
+            });
+            org_markers.push(orgmarker);
+            google.maps.event.addListener(orgmarker, 'click', function() {
+                org_info_window.setContent(
+                    "<a href=\""+ org_link[1] + "\" target=\"_blank\">" + org_link[4] + "</a>"
+                );
+                org_info_window.open(orgmap, orgmarker);
+            });
+        }
+    });
+        
+}
 
   /*
   Parses html from organization page looking for any links in the form of
@@ -139,9 +139,3 @@ var MAPZOOMLEVEL = 3;
     orgmap = createMap(map_div, map_options);
     load_org_links(ORGPAGEURL);
   }
-</script>
-<script type="text/javascript">
-  google.maps.event.addDomListener(window, 'load', function(){
-    load_org_map(MAPCANVASDIV);
-  });
-</script>
